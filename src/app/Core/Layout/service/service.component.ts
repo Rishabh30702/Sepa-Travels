71d5063578451dceef351fa42dbda759633983a8
yadavaman588 <147317740+yadavaman588@users.noreply.github.com>
2024-12-12 18:04:10 +0530

    services page translation

--- src/app/Core/Layout/service/service.component.ts
+++ src/app/Core/Layout/service/service.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-service',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent,TranslateModule],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css'
 })
